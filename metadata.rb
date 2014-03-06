e1d411716262046f986209a0e48fcedfbc3869b9
Darron Froese <darron@froese.org>
2014-03-06 13:52:50 -0700

    First commit.

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+# encoding: utf-8
+name             'postfix_relayhost'
+maintainer       'Darron Froese'
+maintainer_email 'darron@froese.org'
+license          'Apache 2.0'
+description      'Installs/configures postfix_relayhost'
+version          '0.1.0'
+recipe           'postfix_relayhost::default', 'Installs/configures postfix_relayhost'
